328c292a32373c1b8a859166f7025b060aabc6de
Herioos <heribertoar90@gmail.com>
2025-06-17 08:37:46 -0700

    ctm
    asdasd

--- src/main/java/org/example/tinaco/MainApplication.java
+++ src/main/java/org/example/tinaco/MainApplication.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 
 public class MainApplication extends Application {
     //hola pendejo
+    //Ctm
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/login.fxml"));
