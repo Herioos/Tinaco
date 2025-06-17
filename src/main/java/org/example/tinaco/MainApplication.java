05a67172231ba7d50f3644cb3e117a9b95753e34
ANNTUAN <garyazulino4@gmail.com>
2025-06-17 08:35:42 -0700

    cambio xd
    para heri

--- src/main/java/org/example/tinaco/MainApplication.java
+++ src/main/java/org/example/tinaco/MainApplication.java
@@ -8,6 +8,7 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class MainApplication extends Application {
+    //hola pendejo
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("views/login.fxml"));
